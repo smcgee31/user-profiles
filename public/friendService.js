cf52430b2dc66ce90fe2077194e71cc747461128
smcgee31 <solodesign31@gmail.com>
2016-04-24 16:57:14 -0600

    upload of files to keep backup current

--- public/friendService.js
+++ public/friendService.js
@@ -1,13 +1,20 @@
 angular.module('userProfiles')
 .factory('friendService', function( $http ) {
-  return {
-    
-    login: function( user ) {
-      /* FIX ME */
-    },
+    return {
 
-    getFriends: function() {
-    	/* FIX ME */
-    }
-  }
+        login: function( user ) {
+        /* FIX ME */
+            return $http({
+                method: post,
+
+            });
+        },
+
+        getFriends: function() {
+        /* FIX ME */
+        return $http({
+            method: GET,
+        });
+        }
+    };
 });
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cors = require('cors');
+
+var config = require('./config');
+var profileCtrl = require('./controllers/profileCtrl');
+var userCtrl = require('./controllers/userCtrl');
+
+
+var corsOptions = {
+    origin: 'http://localhost:8080'
+};
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(session({
+    secret: config.sessionSecret,
+    saveUninitialized: true,
+    resave: true
+}));
+
+
+app.post('/api/login', userCtrl.login);
+app.get('/api/profiles', profileCtrl.friendArray);
+
+
+
+
+
+
+
+var port = 3000;
+app.listen(3000, function() {
+    console.log('');
+    console.log('>>>O.o<<< Listening for ghosts in the machine on port', port, ' >>>o.O<<<');
+});
